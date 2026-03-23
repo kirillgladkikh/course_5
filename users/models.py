b12800d8209b53e1e57a0c5d51e5e56c159adb37
kirill.gladkikh <k.gladkikh@mail.ru>
2026-03-23 18:28:27 +0300

    users.User add

--- users/models.py
+++ users/models.py
@@ -1,4 +1,24 @@
 from django.contrib.auth.models import AbstractUser
+from django.db.models import (
+    CharField,
+    EmailField,
+)
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = EmailField(unique=True, verbose_name="Email")
+    phone = CharField(max_length=35, verbose_name="Телефон", blank=True, null=True, help_text="Введите номер телефона")
+    country = CharField(max_length=50, verbose_name="Страна", blank=True, null=True, help_text="Введите страну")
+    chat_id = CharField(max_length=255, verbose_name="chat_id", blank=True, null=True, help_text="Введите ТГ ID")
+
+    token = CharField(max_length=100, verbose_name="Token", blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
