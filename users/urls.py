01707581d30a7d427cc142b1ead9cab1bd2b4994
kirill.gladkikh <k.gladkikh@mail.ru>
2026-03-23 18:22:27 +0300

    DRF + users.User fix

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from views import UserIndexView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", UserIndexView.as_view(), name='index'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-def index(request):
-    return HttpResponse("Привет! Это моё первое Django‑приложение.")
+class UserIndexView(APIView):
+    def get(self, request):
+        return Response({"message": "Добро пожаловать в API привычек!"}, status=status.HTTP_200_OK)
