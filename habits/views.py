fbb33c335d95145dd1aeaa788002ae63c9e1f9b0
kirill.gladkikh <k.gladkikh@mail.ru>
2026-03-23 18:23:39 +0300

    habits.Habit init

---
+++ habits/views.py
@@ -0,0 +1,20 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
+from habits.models import Habit
+from habits.paginators import HabitPagination
+from habits.serializers import HabitSerializer
+
+
+# CRUD для модели Habits с использованием ViewSet
+class HabitViewSet(ModelViewSet):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    pagination_class = HabitPagination
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
