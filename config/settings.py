63d48d8c898661f1f6db91dc318429482a29fcf3
kirill.gladkikh <k.gladkikh@mail.ru>
2026-04-07 17:04:23 +0300

    udgrade after consultation

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"  # или DEBUG = True if os.getenv('DEBUG') == "True" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+# ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
